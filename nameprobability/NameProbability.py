817b19b55f228104c8486e543f02f9ea947d7a72
zjelveh <zjelveh@gmail.com>
2015-07-29 14:28:38 -0500

    remove name-cleaver dependency

--- nameprobability/NameProbability.py
+++ nameprobability/NameProbability.py
@@ -2,15 +2,13 @@ import numpy as np
 import os
 from collections import defaultdict
 import Levenshtein as edist
-from name_cleaver import IndividualNameCleaver as indiv, OrganizationNameCleaver as company
 import cPickle
 from counter import _editCounts, _ngramCount, _probName, _condProbName, _probSamePerson
 
 class NameMatcher():
-    def __init__(self, name_list=None, ngram_len=5, smoothing=.001,
-                 standardizeType=None, unique=True, useSS=True, edit_count_max=None):
+    def __init__(self, name_list=None, ngram_len=5, smoothing=.001, unique=True, 
+        useSS=True, edit_count_max=None):
         '''
-        - standardizeType can be set to 'Indiv' or 'Company'
         - edit_count_max is used to limit the number of samples to consider
         when computing edit operation counts
         '''
@@ -20,18 +18,13 @@ class NameMatcher():
         self.ngram_len = ngram_len
         self.pop_size = 0
         self.total_edits = 0
-        self.standardizeFunc = None
         self.unique = unique
         self.edit_count_max = edit_count_max
         self.DATA_PATH = os.path.join(os.path.split(__file__)[0], "data")
         self.memoize = {}
         self.cp_memoize = {}
         self.psp_memoize = {}
-        if standardizeType == 'Indiv':
-                self.standardizeFunc = lambda name: indiv(name).parse(safe=True).name_str().lower()
-        if standardizeType == 'Company':
-                self.standardizeFunc = lambda name: company(name).parse(safe=True).name.lower()
-
+        
         if useSS:
             with open(os.path.join(self.DATA_PATH, 'ss_data.pkl'),'rb') as ss_data:
                 print 'Loading Social Security Data'
@@ -42,8 +35,6 @@ class NameMatcher():
         if name_list:
             if not isinstance(name_list, list):
                 name_list = list(name_list)
-            if standardizeType:
-                name_list = [self.standardizeFunc(name) for name in name_list]
             if self.unique:
                 name_list = list(set(name_list))
 
@@ -59,8 +50,6 @@ class NameMatcher():
             self.ngram_count[k] += v
 
     def updateCounts(self, name_list):
-        if self.standardizeFunc:
-            name_list = [self.standardizeFunc(name) for name in name_list]
         if self.unique:
             name_list = list(set(name_list))
         self.pop_size += len(name_list)
@@ -93,16 +82,12 @@ class NameMatcher():
     def condProbName(self, name1, name2):
         # computes the conditional probability of arriving at name1
         # by performing a series of operation on name2.
-        if self.standardizeFunc:
-            name1, name2 = map(self.standardizeFunc, [name1, name2])
         if (name1, name2) not in self.cp_memoize:
             self.cp_memoize = _condProbName(name1, name2, self.edit_count, self.total_edits,
                                             self.smoothing, self.cp_memoize)
         return self.cp_memoize[(name1, name2)]
 
     def probSamePerson(self, name1, name2):
-        if self.standardizeFunc:
-            name1, name2 = map(self.standardizeFunc, [name1, name2])
         # computes the probability that the two names belong to the same person.
         if len(name1) < self.ngram_len or len(name2) < self.ngram_len:
             print 'Both names should be at least', self.ngram_len, ' characters long'
@@ -116,22 +101,17 @@ class NameMatcher():
         return self.psp_memoize[(name1, name2)]
 
     def probUnique(self, name):
-        if self.standardizeFunc:
-            name = self.standardizeFunc(name)
         # compute the probability that a name is unique in the data
         return 1. / ((self.pop_size - 1) * self.probName(name) + 1)
 
     def surprisal(self, name):
-        if self.standardizeFunc:
-            name = self.standardizeFunc(name)
         return -np.log2(self.probUnique(name))
 
     def loadRandom(self):
-        self.standardizeFunc = lambda name: indiv(name).parse(safe=True).name_str().lower()
         with open(os.path.join(self.DATA_PATH, 'sample_names.csv'), 'rb') as f:
             self.updateCounts(f.read().split('\n'))
 
 if __name__ == '__main__':
     pass
 
-    temp = NameProbability.NameMatcher(name_list=['abc company', 'bcd inc'], standardizeType='Company', useSS=False)
+    temp = NameProbability.NameMatcher(name_list=['abc company', 'bcd inc'], useSS=False)
